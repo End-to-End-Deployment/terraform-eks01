fddaf8bdf1b12fcc5afee71866b10ac49b922cc9
Samir-Kesare <145540651+Samir-Kesare@users.noreply.github.com>
2024-07-30 22:39:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,3 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
-  depends_on  = [module.eks]
-}
-
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-  depends_on  = [module.eks]
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-  depends_on  = [module.eks]
-}
-
-output "region" {
-  description = "AWS region"
-  value       = var.aws_region
-}
-
 output "kubeconfig" {
   value = module.eks.cluster_id != null && module.eks.cluster_endpoint != null && module.eks.cluster_certificate_authority_data != null ? <<EOT
 apiVersion: v1
@@ -53,5 +30,5 @@ users:
       #   - name: AWS_STS_REGIONAL_ENDPOINTS
       #     value: regional
 EOT
-  : ""  # Ensure this is a valid value or expression
+  : "kubeconfig not available due to missing module output values."
 }
