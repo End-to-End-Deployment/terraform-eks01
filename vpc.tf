a05654e5f3977d679bfac327c189ca256c1c3466
Samir-Kesare <145540651+Samir-Kesare@users.noreply.github.com>
2024-07-31 11:06:32 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,14 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_id
+}
+
 locals {
   cluster_name = "abhi-eks-${random_string.suffix.result}"
 }
