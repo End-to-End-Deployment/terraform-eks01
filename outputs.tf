2eb7dc8cd37cb0b9cc816a3c74ca8164d5a4cb7b
Samir-Kesare <145540651+Samir-Kesare@users.noreply.github.com>
2024-07-31 11:54:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,43 +18,7 @@ output "region" {
   value       = var.aws_region
 }
 
-output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+output "update_kubeconfig_command" {
+  description = "Command to update kubeconfig to use the EKS cluster"
+  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_id} --region ${var.aws_region}"
 }
-output "kubeconfig" {
-  description = "Kubeconfig file for accessing the EKS cluster."
-  value = <<EOT
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${data.aws_eks_cluster.cluster.endpoint}
-    certificate-authority-data: ${data.aws_eks_cluster.cluster.certificate_authority[0].data}
-  name: ${module.eks.cluster_id}
-contexts:
-- context:
-    cluster: ${module.eks.cluster_id}
-    user: aws
-  name: ${module.eks.cluster_id}
-current-context: ${module.eks.cluster_id}
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws
-      args:
-        - eks
-        - get-token
-        - --cluster-name
-        - ${module.eks.cluster_id}
-      env:
-        - name: AWS_PROFILE
-          value: "default"
-EOT
-}
-#output "zz_update_kubeconfig_command" {
-  # value = "aws eks update-kubeconfig --name " + module.eks.cluster_id
-#  value = format("%s %s %s %s", "aws eks update-kubeconfig --name", module.eks.cluster_id, "--region", var.aws_region)
-#}
